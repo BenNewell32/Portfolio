9aa37a3f6e72c939d4a11c61954ff33de150b963
BenNewell32 <ben.m.newell@gmail.com>
2018-06-25 10:06:49 -0500

    Begin web scrape

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Portfolio
 Unleash the Benny. RAWR!
+
+##Utilizing
+
+Bootstrap Template
+https://startbootstrap.com/template-overviews/new-age/
+
+express-generator NPM
+https://www.npmjs.com/package/express-generator
+
+express documentation
+http://expressjs.com/en/4x/api.html#res.render
+
+cheerio
+https://scotch.io/tutorials/scraping-the-web-with-node-js
+
---
+++ public/javascripts/LinkedIn.js
@@ -0,0 +1,17 @@
+var url = 'http://www.imdb.com/title/tt1229340/';
+var $;
+var title, release, rating;
+var json;
+var LinkedInData= function(){
+    var url = 'http://www.imdb.com/title/tt1229340/';
+    // request(url, function(error, response, html){
+    //     if(!error){
+    //          $ = cheerio.load(html);
+    //         title, release, rating;
+    //         json = { title : "", release : "", rating : ""};
+    //     }
+    // })
+    console.log(url);
+}
+
+LinkedInData();
