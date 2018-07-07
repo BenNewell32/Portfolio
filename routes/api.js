436454920c737685a5ca336f8970a190ec15ceba
BenNewell32 <ben.m.newell@gmail.com>
2018-07-07 00:24:32 -0500

    add background image to project cards

--- routes/api.js
+++ routes/api.js
@@ -44,7 +44,7 @@ function urlExists(url){
     } else {
         console.log('Exists');
         console.log(url);
-        img= 'background-image: url(../images/'+url+'.png)'
+        img= 'url(../images/'+url+'.png)'
         return img;
     }
 }
