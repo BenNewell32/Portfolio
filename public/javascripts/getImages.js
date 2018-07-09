2cc3f1b778d89339916ec8dda37736d4d386cc28
BenNewell32 <ben.m.newell@gmail.com>
2018-07-08 22:55:54 -0500

    fixed sizing of headers on project page

--- public/javascripts/getImages.js
+++ public/javascripts/getImages.js
@@ -2,27 +2,40 @@ var fetch = require('node-fetch');
 const fs = require('fs');
 const screenshot = require('screenshot-stream');
 
-function checkImage(url,title){
-    fs.exists(title+'.png', (exists) => {
-        console.log(exists ? removeImage(title) : buildImg(title,url));
-      });
+testArray=[
+    {
+        title: 'Portfolio',
+        url: 'www.bens.work'
+    },
+    {
+        title: 'Bananashop',
+        url: 'bananashopv1.herokuapp.com/',
+    }
+]
+
+function checkImage(projectWebsites){
+    projectWebsites.forEach(element => {    
+        fs.exists('./public/images/'+element.title+'.png', (exists) => {
+            console.log(exists ? removeImage(element.title) : buildImg(element.title,element.url));
+        });
+    });
 }
 
 function removeImage(title){
-    var file=(title+'.png').toString();
+    var file=('./public/images/'+title+'.png').toString();
     fs.unlink(file, (err) => {
         if (err) throw err;
-        console.log(title+'.png'+' was deleted');
+        console.log('./public/images/'+title+'.png'+' was deleted');
         buildImg(title);
     });
 }
 
 function buildImg(title,url){
-    const stream = screenshot('http://'+url, '1024x768', {crop: true, delay:10}); 
-    stream.pipe(fs.createWriteStream(title+'.png'));
-    return title+'.png';
+    const stream = screenshot('http://'+url, '1024x768', {crop: true, delay:5}); 
+    stream.pipe(fs.createWriteStream('./public/images/'+title+'.png'));
+    return './public/images/'+title+'.png';
 }
 
-checkImage('www.bens.work','Portfolio');
+checkImage(testArray);
 
 module.exports = {checkImage}
