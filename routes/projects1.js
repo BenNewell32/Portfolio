b2c4cd5b0f71ca740f80ac214f78a1aa1aabc10b
BenNewell32 <ben.m.newell@gmail.com>
2018-09-24 15:55:03 -0500

    update link for tableau javascript api

--- routes/projects1.js
+++ routes/projects1.js
@@ -7,34 +7,10 @@ var fs = require('fs');
 var request = require('request');
 var fetch = require('node-fetch');
 
-var allProjects=[];
-
-function fetchProjects(){
-  allProjects=[];
-  fetch('http://www.bens.work/api')
-      .then(
-          function(response){
-              return response.json();
-          }
-      )
-      .then(
-          function(myJson){
-              allProjects=myJson;
-              console.log('')
-              console.log(allProjects);
-          }
-      )
-  }
-
-fetchProjects();
-
-
 router.get('/', function(req, res, next) {
-    setTimeout(()=>{
-        res.render('projects1', { 
-            title: 'Projects1', 
-            projects: allProjects 
-        })
-    },3000)
+    res.render('projects1', { 
+        title: 'TableauAPI', 
+        projects: allProjects 
+    })
 })
 module.exports = router;
