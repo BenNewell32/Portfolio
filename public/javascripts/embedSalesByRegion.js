64754d2a114670f95fd588633894f38523ed3871
BenNewell32 <ben.m.newell@gmail.com>
2018-09-25 15:08:03 -0500

    Update visualizations.

--- public/javascripts/embedSalesByRegion.js
+++ public/javascripts/embedSalesByRegion.js
@@ -4,7 +4,7 @@ var counter=0;
 function initViz() {
     counter=1;
     var containerDiv = document.getElementById("vizContainer"),
-        url = "https://public.tableau.com/views/SampleSalesbyRegion/Dashboard1?:embed=y&:display_count=yes&publish=yes",
+        url = "https://public.tableau.com/views/Portal-SalesByRegion/Dashboard1?:embed=y&:display_count=yes&publish=yes",
         options = {
             hideTabs: true,
             hideToolbar: false,
